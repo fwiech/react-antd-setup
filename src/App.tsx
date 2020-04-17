2683fccbc8c59e29699435910869089062191585
Florian Wiech <git@florianwiech.me>
2020-04-17 18:01:39 +0200

    update setup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from "react";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+
+export const Home = () => <h1>Home Page</h1>;
+export const Foo = () => <h1>Foo Page</h1>;
+export const Bar = () => <h1>Bar Page</h1>;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Router>
+          <div>
+            <nav>
+              <Link to="/">Home</Link>&nbsp;- &nbsp;
+              <Link to="/foo">Foo</Link>&nbsp;- &nbsp;
+              <Link to="/bar">Bar</Link>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/foo" component={Foo} />
+              <Route exact path="/bar" component={Bar} />
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
